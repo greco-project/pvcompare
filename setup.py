f67760438a560040be6b02faa1f87353b5737558
SabineH <sabine.haas@rl-institut.de>
2020-01-29 16:30:00 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='pvcompare',
+      version='0.1.0dev',
+      description='Evaluating pv technologies in net zero energy communities.',
+      url='https://github.com/greco-project/pvcompare',
+      author='Reiner Lemoine Institut',
+      # author_email='sabine.haas@rl-institut.de',
+      license='MIT',
+      packages=['pvcompare'],
+      package_data={
+          'pvcompare': [os.path.join('data', 'load_profiles', '*.csv'),
+                        os.path.join('data', 'pv', '*.csv')]},
+      long_description=read('README.md'),
+      long_description_content_type='text/x-rst',
+      zip_safe=False,  # todo
+      python_requires=">=3.5, <4",
+      install_requires=["pandas", "pvlib", "demandlib"],
+      extras_require={
+          'dev': ['pytest']})
