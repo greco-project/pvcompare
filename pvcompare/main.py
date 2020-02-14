459e69eb502194dd25833b940efd2d41740928a3
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2020-02-14 16:32:43 +0100

    update pv_feedin function

--- pvcompare/main.py
+++ pvcompare/main.py
@@ -3,12 +3,15 @@
 import pv_feedin
 import pandas as pd
 import logging
-
+import sys
 import era5
 
-log = logging.getLogger()
+# Reconfiguring the logger here will also affect test running in the PyCharm IDE
+log_format = '%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=log_format)
 
-def main(lat, lon, year, input_directory=None, output_directory=None):
+def main(lat, lon, year, population, input_directory=None, output_directory=None,
+         mvs_input_directory=None):
 
     """
     loads weather data for the given year and location, calculates pv feedin
@@ -24,18 +27,11 @@ def main(lat, lon, year, input_directory=None, output_directory=None):
     weather= era5.load_era5_weatherdata(lat=lat, lon=lon, year=year)
 
     pv_feedin.create_pv_timeseries(lat=40.3, lon=5.4, weather=weather,
+                                   population=population,
                                    PV_setup=None, plot=True,
                                    input_directory=input_directory,
                                    output_directory=None)
 
-    optimal_tilt = pv_feedin.get_optimal_pv_angle(lat=40.3)
-    nominal_value_pv = pv_feedin.nominal_values_pv(type='si', area=1323,
-                                         surface_azimuth=180,
-                                         surface_tilt=optimal_tilt)
-
-    nominal_value_cpv = pv_feedin.nominal_values_pv(type='cpv', area=1323,
-                                          surface_azimuth=180,
-                                          surface_tilt=optimal_tilt)
 
 
 
@@ -44,5 +40,5 @@ if __name__ == '__main__':
     latitude=40.3
     longitude=5.4
     year=2015
-
-    main(lat=latitude, lon=longitude, year=2015)
+    population=48000
+    main(lat=latitude, lon=longitude, year=2015, population=population)
