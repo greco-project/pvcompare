1fe0ca102b4aef5af5551b2b874ca201204af1ba
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2020-02-21 13:48:04 +0100

    fix area potential

--- pvcompare/area_potential.py
+++ pvcompare/area_potential.py
@@ -41,15 +41,17 @@ def calculate_area_potential(population, input_directory, surface_type):
     datapath = os.path.join(input_directory,
                             'building_parameters.csv')
 
+    #load input parameters from building_parameters.csv
     bp = pd.read_csv(datapath, index_col=0)
     bp = bp.T
-    population_per_storey = bp.iloc[0]['population per storey']
-    number_houses = bp.iloc[0]['number of houses']
-    floor_area = bp.iloc[0]['total storey area']
-    length_south_facade = bp.iloc[0]['length south facade']
-    length_eastwest_facade = bp.iloc[0]['length eastwest facade']
-    hight_per_storey = bp.iloc[0]['hight storey']
+    population_per_storey = int(bp.iloc[0]['population per storey'])
+    number_of_storeys = int(bp.iloc[0]['number of storeys'])
+    floor_area = int(bp.iloc[0]['total storey area'])
+    length_south_facade = int(bp.iloc[0]['length south facade'])
+    length_eastwest_facade = int(bp.iloc[0]['length eastwest facade'])
+    hight_per_storey = int(bp.iloc[0]['hight storey'])
 
+    number_houses= population/(population_per_storey*number_of_storeys)
     if surface_type == "flat_roof":
         area = floor_area * number_houses
     elif surface_type == "gable_roof":
@@ -60,11 +62,9 @@ def calculate_area_potential(population, input_directory, surface_type):
     # number of storeys for each building
 
     else:
-        storeys = population / population_per_storey / number_houses
-
         # solar panels are only starting from the fourth storey (see Hachem2014)
-        if storeys > 3:
-            used_storeys = storeys - 3
+        if number_of_storeys > 3:
+            used_storeys = number_of_storeys - 3
             south_facade = length_south_facade * hight_per_storey * \
                            used_storeys * number_houses
             eastwest_facade = length_eastwest_facade * hight_per_storey * 2 * \
@@ -78,11 +78,13 @@ def calculate_area_potential(population, input_directory, surface_type):
                 area = eastwest_facade / 100 * 80
         else:
             area = 0
+
     logging.info("The area potential has been calculated to be %s " % area +
-                 " for the surface type %s" % surface_type)
-    return (area)
+                 " qm for the surface type %s" % surface_type)
+    return area
 
-# if __name__ == '__main__':
 
-#    calculate_area_potential(population=48000)
-#    plot_facade_potential()
+if __name__ == '__main__':
+
+    area=calculate_area_potential(population=400, input_directory='./data/inputs/', surface_type='flat_roof')
+    print(area)
