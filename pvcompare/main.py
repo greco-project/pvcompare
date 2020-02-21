e6a84b98f92b4a5a7963158992634dd8fd7f2707
Inia Steinbach <RL-INSTITUT\inia.steinbach@pc-44lin.rl-institut.local>
2020-02-21 13:26:46 +0100

    add demand

--- pvcompare/main.py
+++ pvcompare/main.py
@@ -1,11 +1,11 @@
 
 #import feedinlib.era5 as era
-import pv_feedin
 import pandas as pd
 import logging
 import sys
 from pvcompare import era5
 from pvcompare import demand
+from pvcompare import pv_feedin
 
 # Reconfiguring the logger here will also affect test running in the PyCharm IDE
 log_format = '%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s'
@@ -28,13 +28,23 @@ def main(lat, lon, year, population, country, input_directory=None, output_direc
 
     weather= era5.load_era5_weatherdata(lat=lat, lon=lon, year=year)
 
+    if mvs_input_directory==None:
+        mvs_input_directory="./data/mvs_inputs/"
+
     pv_feedin.create_pv_components(lat=lat, lon=lon, weather=weather,
                                    population=population,
-                                   PV_setup=None, plot=plot,
+                                   PV_setup=None,
+                                   plot=plot,
                                    input_directory=input_directory,
                                    output_directory=output_directory,
                                    mvs_input_directory=mvs_input_directory)
 
+    demand.calculate_load_profiles(country=country,
+                                   population=population,
+                                   year=year,
+                                   input_directory=input_directory,
+                                   mvs_input_directory=mvs_input_directory,
+                                   plot=plot)
 
 
 
