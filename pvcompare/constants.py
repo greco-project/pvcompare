46b8f04c91b5fcaefd9e459ebeaca57ba21d8701
adminlocal <adminlocal@rli-nb-18.rl-institut.local>
2020-05-18 17:05:51 +0200

    fix dummy directory path

--- pvcompare/constants.py
+++ pvcompare/constants.py
@@ -11,5 +11,9 @@ DEFAULT_MVS_INPUT_DIRECTORY = os.path.join(
 DEFAULT_MVS_OUTPUT_DIRECTORY = os.path.join(
     os.path.dirname(__file__), "data/mvs_outputs"
 )
-TEST_DATA_DIRECTORY = os.path.join(os.path.abspath(__file__), "/home/adminlocal/Dokumente/greco_env/pvcompare/tests/test_data/")
-DUMMY_TEST_DATA = os.path.join(os.path.abspath(__file__), "/home/adminlocal/Dokumente/greco_env/pvcompare/tests/test_data/dummy_data")
+TEST_DATA_DIRECTORY = os.path.join(
+    os.path.dirname(os.path.join("", os.pardir)), "tests/test_data/",
+)
+DUMMY_TEST_DATA = os.path.join(
+    os.path.dirname(os.path.join("", os.pardir)), "tests/test_data/dummy_data",
+)
