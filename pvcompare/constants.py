64326915c2fe07460f9d2bbcdd17fd1aceb1d273
adminlocal <adminlocal@rli-nb-18.rl-institut.local>
2020-06-10 12:42:24 +0200

    blacks

--- pvcompare/constants.py
+++ pvcompare/constants.py
@@ -12,9 +12,5 @@ DEFAULT_MVS_INPUT_DIRECTORY = os.path.join(
 DEFAULT_MVS_OUTPUT_DIRECTORY = os.path.join(
     os.path.dirname(__file__), "data/mvs_outputs"
 )
-TEST_DATA_DIRECTORY = os.path.join(
-    REPO_PATH, "tests/test_data/",
-)
-DUMMY_TEST_DATA = os.path.join(
-    REPO_PATH, "tests/test_data/dummy_data/",
-)
+TEST_DATA_DIRECTORY = os.path.join(REPO_PATH, "tests/test_data/",)
+DUMMY_TEST_DATA = os.path.join(REPO_PATH, "tests/test_data/dummy_data/",)
