9f5870657354b12c6e9afea441d64dfe0d108acd
Inia <inia@rli-nb-66.rl-institut.local>
2020-10-13 15:00:33 +0200

    add load weather_data

--- pvcompare/main.py
+++ pvcompare/main.py
@@ -60,7 +60,10 @@ def main(
     # check if weather data already exists
     weather_file = os.path.join(input_directory, f"weatherdata_{country}_{year}.csv")
     if os.path.isfile(weather_file):
-        weather = pd.read_csv(os.path.join(input_directory, f"weatherdata_{country}_{year}.csv"), index_col=0)
+        weather = pd.read_csv(
+            os.path.join(input_directory, f"weatherdata_{country}_{year}.csv"),
+            index_col=0,
+        )
     else:
         # if era5 import works this line can be used
         weather = era5.load_era5_weatherdata(lat=latitude, lon=longitude, year=year)
@@ -126,8 +129,12 @@ def apply_mvs(mvs_input_directory, mvs_output_directory):
 
 if __name__ == "__main__":
 
-    latitude = 48.864716  # Madrid: 40.416775 # berlin: 52.5243700 oslo: 59.9127300 athens: 37.983810, Paris: 48.864716
-    longitude = 2.349014  # M: -3.703790 # berlin 13.4105300 oslo:10.7460900 	athens: 23.727539, paris: 2.349014
+    latitude = (
+        48.864716
+    )  # Madrid: 40.416775 # berlin: 52.5243700 oslo: 59.9127300 athens: 37.983810, Paris: 48.864716
+    longitude = (
+        2.349014
+    )  # M: -3.703790 # berlin 13.4105300 oslo:10.7460900 	athens: 23.727539, paris: 2.349014
     year = 2014
     population = 48000
     country = "France"
