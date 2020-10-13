a3a809e10594f1d5cb84f77b78d6f039cf7003a6
Inia <inia@rli-nb-66.rl-institut.local>
2020-10-13 14:53:29 +0200

    adding data

---
+++ pvcompare/cpv/apply_cpvlib_StaticHybridSystem.py
@@ -0,0 +1,140 @@
+import matplotlib.pyplot as plt
+
+import pvlib
+import pandas as pd
+from cpvlib import cpvlib
+from pvcompare.cpv.inputs import mod_params_cpv, mod_params_flatplate
+
+
+def create_cpv_time_series(lat, lon, weather, surface_azimuth, surface_tilt):
+    """
+
+    creates a time series for a type of cpv module
+
+    :param lat: num
+        latitude
+    :param lon: num
+        longitude
+    :param weather: pd.DataFrame()
+        weather dataframe according to pvlib standards
+    :param surface_azimuth: int
+        surface azimuth
+    :param surface_tilt: int
+        surface tilt
+    :param cpv_type: str
+        possible cpv_types integrated up to this point: "ins", "m300"
+    :return: pd.DataFrame()
+    """
+
+    location = pvlib.location.Location(
+        latitude=lat, longitude=lon, tz="utc"
+    )
+
+    weather.index = pd.to_datetime(weather.index)
+    spa = pvlib.solarposition.spa_python(
+        time=weather.index, latitude=lat, longitude=lon
+    )
+    weather["dni"] = pvlib.irradiance.dirint(
+        weather["ghi"], solar_zenith=spa["zenith"], times=weather.index
+    )
+
+    solar_zenith = location.get_solarposition(weather.index).zenith
+    solar_azimuth = location.get_solarposition(weather.index).azimuth
+
+    #%%
+    # StaticHybridSystem
+    static_hybrid_sys = cpvlib.StaticHybridSystem(
+        surface_tilt=surface_tilt,
+        surface_azimuth=surface_azimuth,
+        module_cpv=None,
+        module_flatplate=None,
+        module_parameters_cpv=mod_params_cpv,
+        module_parameters_flatplate=mod_params_flatplate,
+        modules_per_string=1,
+        strings_per_inverter=1,
+        inverter=None,
+        inverter_parameters=None,
+        racking_model="insulated",
+        losses_parameters=None,
+        name=None,
+    )
+
+    # get_effective_irradiance
+    (
+        weather["dii_effective"],
+        weather["poa_flatplate_static_effective"],
+    ) = static_hybrid_sys.get_effective_irradiance(
+        solar_zenith,
+        solar_azimuth,
+        iam_param=0.7,
+        aoi_limit=60,
+        dii=None,
+        ghi=weather["ghi"],
+        dhi=weather["dhi"],
+        dni=weather["dni"],
+    )
+
+    # pvsyst_celltemp
+    (
+        weather["temp_cell_35"],
+        weather["temp_cell_flatplate"],
+    ) = static_hybrid_sys.pvsyst_celltemp(
+        dii=weather["dii_effective"],
+        poa_flatplate_static=weather["poa_flatplate_static_effective"],
+        temp_air=weather["temp_air"],
+        wind_speed=weather["wind_speed"],
+    )
+
+    weather.fillna(0, inplace=True)
+    # calcparams_pvsyst
+    (
+        diode_parameters_cpv,
+        diode_parameters_flatplate,
+    ) = static_hybrid_sys.calcparams_pvsyst(
+        dii=weather["dii_effective"],
+        poa_flatplate_static=weather["poa_flatplate_static_effective"],
+        temp_cell_cpv=weather["temp_cell_35"],
+        temp_cell_flatplate=weather["temp_cell_flatplate"],
+    )
+
+    # singlediode
+    dc_cpv, dc_flatplate = static_hybrid_sys.singlediode(
+        diode_parameters_cpv, diode_parameters_flatplate
+    )
+
+    # uf_global (uf_am, uf_temp_air)
+    weather["am"] = location.get_airmass(weather.index).airmass_absolute
+
+    uf_cpv = static_hybrid_sys.get_global_utilization_factor_cpv(
+        weather["am"], weather["temp_air"]
+    )
+
+    # Power
+    # dc_cpv.p_mp.plot(label="cpv")
+    # dc_flatplate.p_mp.plot(label="flatplate")
+
+    # Energy
+    energy_cpv = dc_cpv["p_mp"] * uf_cpv
+    energy_flatplate = dc_flatplate["p_mp"]
+
+    # add temperature correction -> not, because its already covered in uf_temp
+
+    total = energy_cpv + energy_flatplate
+
+    import matplotlib.pyplot as plt
+
+    week_cpv = energy_cpv.iloc[4400:4500]
+    week_flatplate = energy_flatplate.iloc[4400:4500]
+    plt.figure(figsize=(15, 10))
+    plt.plot(week_cpv, label="cpv")
+    plt.plot(week_flatplate, label="flatplate")
+    plt.legend()
+    plt.savefig("/home/inia/Dokumente/greco_env/Compare_Technologies/cpv_flatplate.png")
+
+    plt.figure(figsize=(15, 10))
+    plt.plot(weather["dni"].iloc[4400:4500], label="dni")
+    plt.plot(weather["dhi"].iloc[4400:4500], label="dhi")
+    plt.legend()
+    plt.savefig("/home/inia/Dokumente/greco_env/Compare_Technologies/cpv_dni_dhi.png")
+
+    return total
---
+++ pvcompare/cpv/inputs.py
@@ -0,0 +1,97 @@
+import pandas as pd
+
+
+mod_params_cpv = {
+    "gamma_ref": 5.524,
+    "mu_gamma": 0.003,
+    "I_L_ref": 0.96,
+    "I_o_ref": 0.00000000017,
+    "R_sh_ref": 5226,
+    "R_sh_0": 21000,
+    "R_sh_exp": 5.50,
+    "R_s": 0.01,
+    "alpha_sc": 0.00,
+    "EgRef": 3.91,
+    "irrad_ref": 1000,
+    "temp_ref": 25,
+    "cells_in_series": 12,
+    "cells_in_parallel": 48,
+    "eta_m": 0.32,
+    "alpha_absorption": 0.9,
+    "Area": 0.131,
+    "v_mp": 33.5,
+    "i_mp": 0.893,
+    "u_c": 29.0,  # default value from https://pvlib-python.readthedocs.io/en/latest/generated/pvlib.temperature.pvsyst_cell.html#pvlib.temperature.pvsyst_cell
+    "u_v": 0.00,
+    "alpha": 0.11,  # temperature coefficient for the whole module see INS datasheet not used
+}
+
+UF_parameters_cpv = {
+    "IscDNI_top": 0.96 / 1000,
+    "am_thld": 4.574231933073185,
+    "am_uf_m_low": 3.906372068620377e-06,
+    "am_uf_m_high": -3.0335768119184845e-05,
+    "ta_thld": 50,
+    "ta_uf_m_low": 4.6781224141650075e-06,
+    "ta_uf_m_high": 0,
+    "weight_am": 0.2,
+    "weight_temp": 0.8,
+}
+
+mod_params_cpv.update(UF_parameters_cpv)
+
+# example (NO Insolight) PV module from:
+# https://pvpmc.sandia.gov/PVLIB_Matlab_Help/html/pvl_calcparams_PVsyst_help.html
+mod_params_flatplate = {
+    "gamma_ref": 1.1,
+    "mu_gamma": -0.0003,
+    "I_L_ref": 8,
+    "I_o_ref": 2.2e-9,
+    "R_sh_ref": 200,
+    "R_sh_0": 8700,
+    "R_sh_exp": 5.5,
+    "R_s": 0.33,
+    "alpha_sc": -0.002,
+    "EgRef": 1.121,
+    "irrad_ref": 1000,
+    "temp_ref": 25,
+    "cells_in_series": 4,  # 60,
+    "eta_m": 0.95,  # pvsyst_celltemp() default value
+    "alpha_absorption": 0.97,  # pvsyst_celltemp() default value
+    "i_mp": 0.0,
+    "v_mp": 0.0,
+}
+
+
+# elif cpv_type == "m300":
+#
+#     module_params = {
+#         "gamma_ref": 4.456,
+#         "mu_gamma": 0.0012,
+#         "I_L_ref": 3.346,
+#         "I_o_ref": 0.000000000004,
+#         "R_sh_ref": 4400,
+#         "R_sh_0": 17500,
+#         "R_sh_exp": 5.50,
+#         "R_s": 0.736,
+#         "alpha_sc": 0.00,
+#         "irrad_ref": 1000,
+#         "temp_ref": 25,
+#         "cells_in_series": 42,
+#         "v_mp": 116.63,
+#         "i_mp": 3.082,
+#         "Area": 1.269,
+#     }
+#
+#     UF_parameters = {
+#         "IscDNI_top": 1,
+#         "thld_am": 2.022411098853249,
+#         "m_low_am": 0.0423037910485609,
+#         "m_high_am": -0.0210539236615148,
+#         "thld_temp": 200,
+#         "m_low_temp": 0.000923828521724516,
+#         "m_high_temp": 0.0,
+#         "weight_am": 0.2,
+#         "weight_temp": 0.8,
+#     }
+# module_params.update(UF_parameters)
