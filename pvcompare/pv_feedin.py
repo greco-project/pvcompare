33aa44a213b5dee38c10ac897718e99905231d83
Piranias <44803304+Piranias@users.noreply.github.com>
2020-10-27 17:41:05 +0100

    Update pvcompare/pv_feedin.py
    Co-authored-by: Sabine Haas <sabine.haas@rl-institut.de>

--- pvcompare/pv_feedin.py
+++ pvcompare/pv_feedin.py
@@ -91,7 +91,7 @@ def create_pv_components(
     normalization: str
         "NP": Normalize by peak power
         "NSTC": Normalize by reference p_mp
-        "NRWCL": Normalize by realworld p_mp
+        "NRWC": Normalize by realworld p_mp
         None: no normalization
 
 
