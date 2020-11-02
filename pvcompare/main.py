757ece3b194465e2c289a15b8c322ec02cea2038
Inia <inia@rli-nb-66.rl-institut.local>
2020-11-02 18:16:34 +0100

    apply black

--- pvcompare/main.py
+++ pvcompare/main.py
@@ -50,9 +50,9 @@ def main(
     if mvs_input_directory == None:
         mvs_input_directory = constants.DEFAULT_MVS_INPUT_DIRECTORY
 
-#    if all([latitude, longitude, country, year]) == False:
+    #    if all([latitude, longitude, country, year]) == False:
     check_inputs.add_project_data(
-            mvs_input_directory, latitude, longitude, country, year
+        mvs_input_directory, latitude, longitude, country, year
     )
     check_inputs.add_electricity_price()
 
@@ -127,7 +127,9 @@ if __name__ == "__main__":
 
     latitude = 40.416775  # Madrid: 40.416775 # berlin: 52.5243700 oslo: 59.9127300 athens: 37.983810, Paris: 48.864716
 
-    longitude = -3.703790  # M: -3.703790 # berlin 13.4105300 oslo:10.7460900 	athens: 23.727539, paris: 2.349014
+    longitude = (
+        -3.703790
+    )  # M: -3.703790 # berlin 13.4105300 oslo:10.7460900 	athens: 23.727539, paris: 2.349014
     year = 2014
     population = 48000
     country = "Spain"
