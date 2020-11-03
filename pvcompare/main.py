0750dc053fde8b8165116f51642e158f3caf8d31
Inia <inia@rli-nb-66.rl-institut.local>
2020-11-03 10:00:30 +0100

    apply black

--- pvcompare/main.py
+++ pvcompare/main.py
@@ -127,9 +127,7 @@ if __name__ == "__main__":
 
     latitude = 52.5243700  # Madrid: 40.416775 # berlin: 52.5243700 oslo: 59.9127300 athens: 37.983810, Paris: 48.864716
 
-    longitude = (
-        13.4105300
-    )  # M: -3.703790 # berlin 13.4105300 oslo:10.7460900 	athens: 23.727539, paris: 2.349014
+    longitude = 13.4105300  # M: -3.703790 # berlin 13.4105300 oslo:10.7460900 	athens: 23.727539, paris: 2.349014
     year = 2014
     population = 48000
     country = "Germany"
