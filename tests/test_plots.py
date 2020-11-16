fbb32a663a96b73e72d2d3fbbd97d94a328143fc
Inia <inia@rli-nb-66.rl-institut.local>
2020-11-16 14:35:39 +0100

    apply black

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -9,9 +9,8 @@ https://docs.python.org/3/library/unittest.html are also good support.
 """
 
 import os
-from pvcompare.plots import (
-    plot_all_flows,
-    plot_kpi_loop)
+from pvcompare.plots import plot_all_flows, plot_kpi_loop
+
 
 class TestPlotProfiles:
     @classmethod
@@ -21,14 +20,17 @@ class TestPlotProfiles:
             os.path.dirname(__file__), "test_data/test_mvs_outputs/"
         )
         self.timeseries_directory = os.path.join(
-            os.path.dirname(__file__), "test_data/test_mvs_outputs/timeseries/")
+            os.path.dirname(__file__), "test_data/test_mvs_outputs/timeseries/"
+        )
 
     def test_plot_all_flows(self):
         """ """
         timeseries_name = "timeseries_all_busses_02.xlsx"
         period = "week"
 
-        filename = os.path.join(self.output_directory, f"plot_{timeseries_name}{period}.png")
+        filename = os.path.join(
+            self.output_directory, f"plot_{timeseries_name}{period}.png"
+        )
         if os.path.exists(filename):
             os.remove(filename)
 
@@ -39,22 +41,22 @@ class TestPlotProfiles:
             timeseries_name=timeseries_name,
         )
 
-        assert os.path.exists(filename
-        )
-
+        assert os.path.exists(filename)
 
     def test_plot_kpi_loop(self):
         """ """
         variable_name = "number of storeys"
 
-        filename = os.path.join(self.output_directory, "plot_scalars_" + str(variable_name) + ".png")
+        filename = os.path.join(
+            self.output_directory, "plot_scalars_" + str(variable_name) + ".png"
+        )
         if os.path.exists(filename):
             os.remove(filename)
 
-        plot_kpi_loop(variable_name = variable_name, kpi = [
-            "costs total PV",
-            "Degree of autonomy"],
-                      loop_output_directory=self.output_directory)
-
-        assert os.path.exists(filename
+        plot_kpi_loop(
+            variable_name=variable_name,
+            kpi=["costs total PV", "Degree of autonomy"],
+            loop_output_directory=self.output_directory,
         )
+
+        assert os.path.exists(filename)
