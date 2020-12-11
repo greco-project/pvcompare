f1bca617f92ac22621ef4ef0227d22c5e57f9c54
Inia <inia@rli-nb-66.rl-institut.local>
2020-12-11 12:40:42 +0100

    apply black

--- run_pvcompare.py
+++ run_pvcompare.py
@@ -1,7 +1,7 @@
 from pvcompare import main
 
 
-#DEFINE USER INPUTS
+# DEFINE USER INPUTS
 latitude = 52.5243700  # Madrid: 40.416775 # berlin: 52.5243700 oslo: 59.9127300 athens: 37.983810, Paris: 48.864716
 longitude = 13.4105300  # M: -3.703790 # berlin 13.4105300 oslo:10.7460900 	athens: 23.727539, paris: 2.349014
 year = 2014
@@ -10,19 +10,19 @@ country = "Germany"
 scenario_name = "Scenario_Z1"
 
 # DEFAULT PARAMETERS
-input_directory=None
-mvs_input_directory=None
-pv_setup=None
-output_directory=None
-mvs_input_directory=None
+input_directory = None
+mvs_input_directory = None
+pv_setup = None
+output_directory = None
+mvs_input_directory = None
 
-#RUN PVCOMPARE PRE-CALCULATIONS:
+# RUN PVCOMPARE PRE-CALCULATIONS:
 # - calculate PV timeseries
 # - if sectorcoupling: calculate heat pump generation
 # - calculate electricity and heat demand
 
 main.apply_pvcompare(
-    population = population,
+    population=population,
     country=country,
     latitude=latitude,
     longitude=longitude,
@@ -33,8 +33,9 @@ main.apply_pvcompare(
     pv_setup=pv_setup,
 )
 
-#RUN MVS OEMOF SIMULATTION
+# RUN MVS OEMOF SIMULATTION
 main.apply_mvs(
-    scenario_name=scenario_name, output_directory=output_directory,
-    mvs_input_directory=mvs_input_directory
+    scenario_name=scenario_name,
+    output_directory=output_directory,
+    mvs_input_directory=mvs_input_directory,
 )
