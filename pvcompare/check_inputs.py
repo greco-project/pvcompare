6ef1f1febf9c8a9102b7443cc21618977037b5e8
Inia <inia@rli-nb-66.rl-institut.local>
2021-01-05 18:13:09 +0100

    small corrections at check_inputs

--- pvcompare/check_inputs.py
+++ pvcompare/check_inputs.py
@@ -267,10 +267,11 @@ def add_electricity_price(static_inputs_directory, user_inputs_mvs_directory):
 
 
 def overwrite_mvs_energy_production_file(
+    pv_setup,
     user_inputs_mvs_directory,
     user_inputs_pvcompare_directory,
     overwrite_pv_parameters,
-    collections_mvs_input_directory=None,
+    collections_mvs_input_directory,
 ):
     """
     Inserts default values for PV technologies defined in pv_setup.csv
@@ -294,9 +295,10 @@ def overwrite_mvs_energy_production_file(
     None
     """
     # load pv_setup.csv
-    pv_setup = pd.read_csv(
-        os.path.join(user_inputs_pvcompare_directory, "pv_setup.csv"), index_col=0
-    )
+    if pv_setup is None:
+        pv_setup = pd.read_csv(
+            os.path.join(user_inputs_pvcompare_directory, "pv_setup.csv"), index_col=0
+        )
     technologies = pv_setup["technology"].values
 
     # load mvs user input file
