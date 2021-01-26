7166e2ddab04a987812153fbe22600aa2a71e9f6
Marie-Claire <marie-claire.gering@rl-institut.de>
2021-01-26 12:34:27 +0100

    Add test of two different ways of implementing a stratified thermal storage to test_stratified_thermal_storage.py

--- tests/test_stratified_thermal_storage.py
+++ tests/test_stratified_thermal_storage.py
@@ -2,9 +2,273 @@ import pytest
 import pandas as pd
 import numpy as np
 import os
+import pvcompare.constants as constants
+import pvcompare.main as main
+import argparse
+import mock
+import shutil
 from pvcompare import stratified_thermal_storage as sts
 from pvcompare.constants import TEST_USER_INPUTS_MVS
 from pvcompare.constants import TEST_USER_INPUTS_PVCOMPARE
+
+EXECUTE_TESTS_ON = os.environ.get("EXECUTE_TESTS_ON", "skip")
+EXECUTE_TESTS_ON = "master"
+TESTS_ON_MASTER = "master"
+
+
+class TestTES:
+    @classmethod
+    def setup_class(self):
+        self.country = "Germany"
+        self.year = 2017
+        self.lat = 53.2
+        self.lon = 13.2
+        self.storeys = 5
+        self.user_inputs_mvs_directory = constants.TEST_USER_INPUTS_MVS
+        self.user_inputs_pvcompare_directory = constants.TEST_USER_INPUTS_PVCOMPARE
+        self.static_inputs_directory = constants.TEST_STATIC_INPUTS
+        self.outputs_directory = constants.TEST_OUTPUTS_DIRECTORY
+        self.scenario_name = "Test_Scenario_TES"
+        self.scenario_name_with_TES_bus = "Test_Scenario_TES_with_TES_bus"
+
+        self.pv_setup = pd.read_csv(
+            os.path.join(self.user_inputs_pvcompare_directory, "pv_setup.csv"),
+            header=0,
+            index_col="technology",
+        )
+        try:
+            self.pv_setup = self.pv_setup.drop(["cpv", "psi"])
+            self.pv_setup.reset_index(drop=False, inplace=True)
+        except KeyError:
+            print(
+                "Please check the format of pv_setup.csv file. The tests might not run correctly."
+            )
+
+        # Save original states of csvs
+        self.mvs_csv_inputs_path = os.path.join(
+            self.user_inputs_mvs_directory, "csv_elements"
+        )
+        self.energy_production_original = pd.read_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyProduction.csv"),
+            header=0,
+            index_col=0,
+        )
+        self.energy_storage_original = pd.read_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyStorage.csv"),
+            header=0,
+            index_col=0,
+        )
+        self.storage_xx_original = pd.read_csv(
+            os.path.join(self.mvs_csv_inputs_path, "storage_TES.csv"),
+            header=0,
+            index_col=0,
+        )
+        self.energy_conversion_original = pd.read_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyConversion.csv"),
+            header=0,
+            index_col=0,
+        )
+        self.energy_busses_original = pd.read_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyBusses.csv"),
+            header=0,
+            index_col=0,
+        )
+
+    # # this ensure that the test is only ran if explicitly executed, ie not when the `pytest` command
+    # # alone is called
+    @pytest.mark.skipif(
+        EXECUTE_TESTS_ON not in (TESTS_ON_MASTER),
+        reason="Benchmark test deactivated, set env variable "
+        "EXECUTE_TESTS_ON to 'master' to run this test",
+    )
+    @mock.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace())
+    def test_implementations_tes(self, margs):
+        """
+        This test checks if the results of two different ways of implementing a stratified
+        thermal storage match.
+        You can include a stratified thermal storage in the model using two ways:
+
+        1. With storage component with `inflow_direction` and `outflow_direction` to the heat bus
+        2. With a storage bus, a storage component and `inflow_direction` and `outflow_direction`
+            as Transformer feeding in and from that bus
+            Please note that the cost parameters of `inflow_direction` and `outflow_direction` of
+            the Transformer should be set to zero, since they cannot be assigned to a real plant
+            component with cost parameters
+        Parameters
+        ----------
+        margs
+
+        """
+        # Simulation of first way of implementation
+        main.apply_pvcompare(
+            storeys=self.storeys,
+            country=self.country,
+            latitude=self.lat,
+            longitude=self.lon,
+            year=self.year,
+            static_inputs_directory=self.static_inputs_directory,
+            user_inputs_pvcompare_directory=self.user_inputs_pvcompare_directory,
+            user_inputs_mvs_directory=self.user_inputs_mvs_directory,
+            plot=False,
+            pv_setup=self.pv_setup,
+            overwrite_grid_costs=True,
+            overwrite_pv_parameters=True,
+        )
+
+        # RUN MVS OEMOF SIMULATTION
+        main.apply_mvs(
+            scenario_name=self.scenario_name,
+            outputs_directory=self.outputs_directory,
+            user_inputs_mvs_directory=self.user_inputs_mvs_directory,
+        )
+
+        # Read results of simulation with first way of implementation
+        results_storage_heat_bus = pd.read_excel(
+            os.path.join(
+                self.outputs_directory,
+                self.scenario_name,
+                "mvs_outputs",
+                "timeseries_all_busses.xlsx",
+            ),
+            sheet_name="Heat bus",
+        )
+
+        # Revert changes made in storage_xx.csv file
+        self.storage_xx_original.to_csv(
+            os.path.join(self.mvs_csv_inputs_path, "storage_TES.csv"), na_rep="NaN"
+        )
+
+        # Calculated files in time_series directory need to be deleted to ensure
+        # the next simulation is run completely
+        time_series_directory = os.path.join(
+            self.user_inputs_mvs_directory, "time_series"
+        )
+        time_series = os.listdir(time_series_directory)
+        for file_name in time_series:
+            if file_name != "file_exists.csv":
+                os.remove(os.path.join(time_series_directory, file_name))
+
+        # Modify mvs csv input data as a preparation for the simulation of
+        # the second way of implementation (see below)
+
+        # Add "TES bus" to busses
+        energy_busses = self.energy_busses_original.copy()
+        energy_busses["TES bus"] = "Heat"
+        energy_busses.to_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyBusses.csv"), na_rep="NaN"
+        )
+        # Add TES input and output converter to transformers
+        energy_conversion = self.energy_conversion_original.copy()
+        energy_conversion["TES converter in"] = [
+            "kW",
+            "True",
+            "None",
+            0,
+            0,
+            30,
+            0,
+            0,
+            0,
+            0,
+            1,
+            "Heat bus",
+            "TES bus",
+            "Heat",
+            "transformer",
+        ]
+        energy_conversion["TES converter out"] = [
+            "kW",
+            "True",
+            "None",
+            0,
+            0,
+            30,
+            0,
+            0,
+            0,
+            0,
+            1,
+            "TES bus",
+            "Heat bus",
+            "Heat",
+            "transformer",
+        ]
+        energy_conversion.to_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyConversion.csv"), na_rep="NaN"
+        )
+        # Adapt inflow and outflow direction of storage to "TES bus"
+        energy_storage = self.energy_storage_original.copy()
+        energy_storage.at["inflow_direction", "storage_TES"] = "TES bus"
+        energy_storage.at["outflow_direction", "storage_TES"] = "TES bus"
+        energy_storage.to_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyStorage.csv"), na_rep="NaN"
+        )
+
+        # Simulation of second way of implementation
+        main.apply_pvcompare(
+            storeys=self.storeys,
+            country=self.country,
+            latitude=self.lat,
+            longitude=self.lon,
+            year=self.year,
+            static_inputs_directory=self.static_inputs_directory,
+            user_inputs_pvcompare_directory=self.user_inputs_pvcompare_directory,
+            user_inputs_mvs_directory=self.user_inputs_mvs_directory,
+            plot=False,
+            pv_setup=self.pv_setup,
+            overwrite_grid_costs=True,
+            overwrite_pv_parameters=True,
+        )
+
+        # RUN MVS OEMOF SIMULATTION
+        main.apply_mvs(
+            scenario_name=self.scenario_name_with_TES_bus,
+            outputs_directory=self.outputs_directory,
+            user_inputs_mvs_directory=self.user_inputs_mvs_directory,
+        )
+
+        # Read results of simulation with second way of implementation
+        results_storage_tes_bus = pd.read_excel(
+            os.path.join(
+                self.outputs_directory,
+                self.scenario_name_with_TES_bus,
+                "mvs_outputs",
+                "timeseries_all_busses.xlsx",
+            ),
+            sheet_name="Heat bus",
+        )
+
+        # Assert that results of both implementations match
+        assert (
+            results_storage_heat_bus["TES input power"].values.all()
+            == results_storage_tes_bus["TES converter in"].values.all()
+        )
+        assert (
+            results_storage_heat_bus["TES output power"].values.all()
+            == results_storage_tes_bus["TES converter out"].values.all()
+        )
+
+        # Revert changes made in mvs csv input files
+        self.storage_xx_original.to_csv(
+            os.path.join(self.mvs_csv_inputs_path, "storage_TES.csv"), na_rep="NaN"
+        )
+        self.energy_busses_original.to_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyBusses.csv"), na_rep="NaN"
+        )
+        self.energy_conversion_original.to_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyConversion.csv"), na_rep="NaN"
+        )
+        self.energy_storage_original.to_csv(
+            os.path.join(self.mvs_csv_inputs_path, "energyStorage.csv"), na_rep="NaN"
+        )
+
+    def teardown_method(self):
+        # delete directory
+        scenarios = [self.scenario_name, self.scenario_name_with_TES_bus]
+        for scenario in scenarios:
+            outputs_directory = os.path.join(self.outputs_directory, scenario)
+            if os.path.exists(outputs_directory):
+                shutil.rmtree(outputs_directory)
 
 
 class TestCalcStratTesParam:
