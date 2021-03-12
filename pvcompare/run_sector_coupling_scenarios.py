59d4550bbaa1fd35dd6b7648a9e045c3616fcdd6
Marie-Claire <marie-claire.gering@rl-institut.de>
2021-03-12 09:36:52 +0100

    Update run_sector_coupling_scenarios.py

--- pvcompare/run_sector_coupling_scenarios.py
+++ pvcompare/run_sector_coupling_scenarios.py
@@ -5,6 +5,97 @@ import os
 import pandas as pd
 
 
+class Scenarios:
+    @classmethod
+    def setup_class(self):
+        # DEFINE USER INPUTS
+        # For scenarios in germany
+        self.latitude_germany = 52.5243700
+        self.longitude_germany = 13.4105300
+        self.years_germany = [
+            2011,
+            2013,
+            2016,
+        ]  # 2011 (good), 2013 (bad), 2016 (medium)
+        self.country_germany = "Germany"
+
+        # For scenarios in spain
+        self.latitude_spain = 40.416775
+        self.longitude_spain = -3.703790
+        self.years_spain = [2013, 2015, 2017]  # 2017 (good), 2013 (bad), 2015 (medium)
+        self.country_spain = "Spain"
+
+        # Building parameter
+        self.storeys = 5
+        self.index_include_ww = 9
+
+        # Set paths
+        self.user_inputs_pvcompare_directory = (
+            constants.DEFAULT_USER_INPUTS_PVCOMPARE_DIRECTORY
+        )
+        self.user_inputs_mvs_directory = constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY
+
+    def run_scenario_a1(self):
+        """
+
+        :return:
+        """
+
+        scenario_name = "Scenario_A1"
+        data_path = os.path.join(self.user_inputs_pvcompare_directory, "pv_setup.csv")
+        # load input parameters from pv_setup.csv
+        pv_setup = pd.read_csv(data_path)
+        pv_setup.at[0, "technology"] = str("si")
+        pv_setup.to_csv(data_path, index=False)
+
+        loop_type = "storeys"
+        loop_dict = {"start": 5, "stop": 5, "step": 1}
+
+        analysis.loop_pvcompare(
+            scenario_name=scenario_name,
+            latitude=self.latitude_germany,
+            longitude=self.longitude_germany,
+            years=self.years_germany,
+            storeys=self.storeys,
+            country=self.country_germany,
+            loop_type=loop_type,
+            loop_dict=loop_dict,
+            user_inputs_mvs_directory=self.user_inputs_mvs_directory,
+            outputs_directory=None,
+            user_inputs_pvcompare_directory=None,
+        )
+
+    def run_scenario_a2(self):
+        """
+
+        :return:
+        """
+
+        scenario_name = "Scenario_A2"
+        data_path = os.path.join(self.user_inputs_pvcompare_directory, "pv_setup.csv")
+        # load input parameters from pv_setup.csv
+        pv_setup = pd.read_csv(data_path)
+        pv_setup.at[0, "technology"] = str("si")
+        pv_setup.to_csv(data_path, index=False)
+
+        loop_type = "storeys"
+        loop_dict = {"start": 5, "stop": 5, "step": 1}
+
+        analysis.loop_pvcompare(
+            scenario_name=scenario_name,
+            latitude=self.latitude_spain,
+            longitude=self.longitude_spain,
+            years=self.years_spain,
+            storeys=self.storeys,
+            country=self.country_spain,
+            loop_type=loop_type,
+            loop_dict=loop_dict,
+            user_inputs_mvs_directory=self.user_inputs_mvs_directory,
+            outputs_directory=None,
+            user_inputs_pvcompare_directory=None,
+        )
+
+
 class ScenariosSectorCoupling:
     @classmethod
     def setup_class(self):
@@ -1527,28 +1618,369 @@ class ScenariosSectorCoupling:
                 os.remove(os.path.join(time_series_path, file))
 
 
+class ScenariosGas:
+    @classmethod
+    def setup_class(self):
+        # DEFINE USER INPUTS
+        # For scenarios in germany
+        self.latitude_germany = 52.5243700
+        self.longitude_germany = 13.4105300
+        self.years_germany = [
+            2011,
+            2013,
+            2016,
+        ]  # 2011 (good), 2013 (bad), 2016 (medium)
+        self.country_germany = "Germany"
+
+        # For scenarios in spain
+        self.latitude_spain = 40.416775
+        self.longitude_spain = -3.703790
+        self.years_spain = [2013, 2015, 2017]  # 2017 (good), 2013 (bad), 2015 (medium)
+        self.country_spain = "Spain"
+
+        # Building parameter
+        self.storeys = 5
+        self.index_include_ww = 9
+
+        # Set paths
+        self.user_inputs_pvcompare_directory = (
+            constants.DEFAULT_USER_INPUTS_PVCOMPARE_DIRECTORY
+        )
+        self.user_inputs_mvs_directory = constants.DEFAULT_USER_INPUTS_MVS_DIRECTORY
+        self.mvs_inputs_path_gas = os.path.abspath(
+            os.path.join(
+                self.user_inputs_mvs_directory,
+                os.pardir,
+                "mvs_inputs_sector_coupling_gas",
+            )
+        )
+
+        def read_data(filename, directory):
+            if directory == "mvs":
+                return pd.read_csv(
+                    os.path.join(self.mvs_inputs_path_gas, "csv_elements", filename),
+                    header=0,
+                    index_col=0,
+                )
+            elif directory == "pvcompare":
+                return pd.read_csv(
+                    os.path.join(self.user_inputs_pvcompare_directory, filename),
+                    header=0,
+                    index_col=0,
+                )
+
+        # Read mvs input data which varies depending on the scenario
+        self.energy_providers_original = read_data("energyProviders.csv", "mvs")
+
+        # Read pvcompare input data which varies depending on the scenario
+        self.building_parameters_original = read_data(
+            "building_parameters.csv", "pvcompare"
+        )
+        self.pv_setup_original = read_data("pv_setup.csv", "pvcompare")
+
+        self.mvs_filenames = [
+            "energyProviders.csv",
+        ]
+
+        self.mvs_files_original = [
+            self.energy_providers_original,
+        ]
+
+        self.pvcompare_filenames = [
+            "building_parameters.csv",
+            "pv_setup.csv",
+        ]
+
+        self.pvcompare_files_original = [
+            self.building_parameters_original,
+            self.pv_setup_original,
+        ]
+
+    def get_data(self):
+        """
+        This function loads data, which will be modified
+        depending on the scenario
+
+        Returns
+        -------
+        mvs_files_modified_list : List of mvs input files
+        pvcompare_files_modified_list : List of pvcompare input files
+        """
+        # Load data to be modified
+        energy_providers = self.energy_providers_original.copy()
+
+        building_parameters = self.building_parameters_original.copy()
+        pv_setup = self.pv_setup_original.copy()
+
+        # Gather data to modify in list
+        mvs_files_modified_list = [
+            energy_providers,
+        ]
+        pvcompare_files_modified_list = [
+            building_parameters,
+            pv_setup,
+        ]
+        return mvs_files_modified_list, pvcompare_files_modified_list
+
+    def save_data(self, mvs_files_modified_list, pvcompare_files_modified_list):
+        """
+        This function saves modified data
+
+        Parameters
+        ----------
+        mvs_files_modified_list : List of mvs input files
+        pvcompare_files_modified_list : List of pvcompare input files
+        """
+        mvs_files_modified = dict(zip(self.mvs_filenames, mvs_files_modified_list))
+        for name, file in mvs_files_modified.items():
+            file.to_csv(
+                os.path.join(self.mvs_inputs_path_gas, "csv_elements", name),
+                na_rep="NaN",
+            )
+
+        pvcompare_files_modified = dict(
+            zip(self.pvcompare_filenames, pvcompare_files_modified_list)
+        )
+        for name, file in pvcompare_files_modified.items():
+            file.to_csv(
+                os.path.join(self.user_inputs_pvcompare_directory, name), na_rep="NaN"
+            )
+
+    def run_scenario_G1(self):
+        """
+        Scenario description:
+            - Sector-coupled
+            - Gas plant
+            - Germany
+            - Rooftop
+            - Standard Si - PV
+            - Heat demand for room heating only
+        """
+
+        scenario_name = "Scenario_G1"
+
+        # Get data
+        (
+            mvs_files_modified_list,
+            pvcompare_files_modified_list,
+        ) = scenarios_gas.get_data()
+
+        # Modify data
+        pvcompare_files_modified_list[0].at["include warm water", "value"] = False
+        pvcompare_files_modified_list[1].at["flat_roof", "technology"] = str("si")
+
+        # Save modified data
+        scenarios_gas.save_data(mvs_files_modified_list, pvcompare_files_modified_list)
+
+        # Begin simulation
+        loop_type = "storeys"
+        loop_dict = {"start": 5, "stop": 5, "step": 1}
+
+        analysis.loop_pvcompare(
+            scenario_name=scenario_name,
+            latitude=self.latitude_germany,
+            longitude=self.longitude_germany,
+            years=self.years_germany,
+            storeys=self.storeys,
+            country=self.country_germany,
+            loop_type=loop_type,
+            loop_dict=loop_dict,
+            user_inputs_mvs_directory=self.mvs_inputs_path_gas,
+            outputs_directory=None,
+            user_inputs_pvcompare_directory=None,
+        )
+
+    def run_scenario_G2(self):
+        """
+        Scenario description:
+            - Sector-coupled
+            - Gas plant
+            - Spain
+            - Rooftop
+            - Standard Si - PV
+            - Heat demand for room heating only
+        """
+
+        scenario_name = "Scenario_G2"
+
+        # Get data
+        (
+            mvs_files_modified_list,
+            pvcompare_files_modified_list,
+        ) = scenarios_gas.get_data()
+
+        # Modify data
+        pvcompare_files_modified_list[0].at["include warm water", "value"] = False
+        pvcompare_files_modified_list[1].at["flat_roof", "technology"] = str("si")
+
+        # Save modified data
+        scenarios_gas.save_data(mvs_files_modified_list, pvcompare_files_modified_list)
+
+        # Begin simulation
+        loop_type = "storeys"
+        loop_dict = {"start": 5, "stop": 5, "step": 1}
+
+        analysis.loop_pvcompare(
+            scenario_name=scenario_name,
+            latitude=self.latitude_spain,
+            longitude=self.longitude_spain,
+            years=self.years_spain,
+            storeys=self.storeys,
+            country=self.country_spain,
+            loop_type=loop_type,
+            loop_dict=loop_dict,
+            user_inputs_mvs_directory=self.mvs_inputs_path_gas,
+            outputs_directory=None,
+            user_inputs_pvcompare_directory=None,
+        )
+
+    def run_scenario_G3(self):
+        """
+        Scenario description:
+            - Sector-coupled
+            - Gas plant
+            - Germany
+            - Rooftop
+            - Standard Si - PV
+            - Heat demand for room heating and warm water
+        """
+
+        scenario_name = "Scenario_G3"
+
+        # Get data
+        (
+            mvs_files_modified_list,
+            pvcompare_files_modified_list,
+        ) = scenarios_gas.get_data()
+
+        # Modify data
+        pvcompare_files_modified_list[0].at["include warm water", "value"] = True
+        pvcompare_files_modified_list[1].at["flat_roof", "technology"] = str("si")
+
+        # Save modified data
+        scenarios_gas.save_data(mvs_files_modified_list, pvcompare_files_modified_list)
+
+        # Begin simulation
+        loop_type = "storeys"
+        loop_dict = {"start": 5, "stop": 5, "step": 1}
+
+        analysis.loop_pvcompare(
+            scenario_name=scenario_name,
+            latitude=self.latitude_germany,
+            longitude=self.longitude_germany,
+            years=self.years_germany,
+            storeys=self.storeys,
+            country=self.country_germany,
+            loop_type=loop_type,
+            loop_dict=loop_dict,
+            user_inputs_mvs_directory=self.mvs_inputs_path_gas,
+            outputs_directory=None,
+            user_inputs_pvcompare_directory=None,
+        )
+
+    def run_scenario_G4(self):
+        """
+        Scenario description:
+            - Sector-coupled
+            - Gas plant
+            - Spain
+            - Rooftop
+            - Standard Si - PV
+            - Heat demand for room heating and warm water
+        """
+
+        scenario_name = "Scenario_G4"
+
+        # Get data
+        (
+            mvs_files_modified_list,
+            pvcompare_files_modified_list,
+        ) = scenarios_gas.get_data()
+
+        # Modify data
+        pvcompare_files_modified_list[0].at["include warm water", "value"] = True
+        pvcompare_files_modified_list[1].at["flat_roof", "technology"] = str("si")
+
+        # Save modified data
+        scenarios_gas.save_data(mvs_files_modified_list, pvcompare_files_modified_list)
+
+        # Begin simulation
+        loop_type = "storeys"
+        loop_dict = {"start": 5, "stop": 5, "step": 1}
+
+        analysis.loop_pvcompare(
+            scenario_name=scenario_name,
+            latitude=self.latitude_spain,
+            longitude=self.longitude_spain,
+            years=self.years_spain,
+            storeys=self.storeys,
+            country=self.country_spain,
+            loop_type=loop_type,
+            loop_dict=loop_dict,
+            user_inputs_mvs_directory=self.mvs_inputs_path_gas,
+            outputs_directory=None,
+            user_inputs_pvcompare_directory=None,
+        )
+
+    def revert_inputs_changes(self):
+        mvs_files = dict(zip(self.mvs_filenames, self.mvs_files_original))
+        pvcompare_files = dict(
+            zip(self.pvcompare_filenames, self.pvcompare_files_original)
+        )
+
+        for name, file in mvs_files.items():
+            file.to_csv(os.path.join(self.mvs_inputs_path_gas, "csv_elements", name),)
+
+        for name, file in pvcompare_files.items():
+            file.to_csv(os.path.join(self.user_inputs_pvcompare_directory, name),)
+
+        time_series_path = os.path.join(self.mvs_inputs_path_gas, "time_series")
+        time_series_files = os.listdir(time_series_path)
+
+        for file in time_series_files:
+            if os.path.exists(os.path.join(time_series_path, file)):
+                os.remove(os.path.join(time_series_path, file))
+
+
 if __name__ == "__main__":
+
+    scenarios = Scenarios()
+    scenarios.setup_class()
+
+    # scenarios.run_scenario_a1()
+    # scenarios.run_scenario_a2()
 
     scenarios_sector_coupling = ScenariosSectorCoupling()
     scenarios_sector_coupling.setup_class()
+    #
+    # scenarios_sector_coupling.run_scenario_E1()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_E2()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_E3()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_E4()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_E5()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_E6()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_F1()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_F2()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_F3()
+    # scenarios_sector_coupling.revert_inputs_changes()
+    # scenarios_sector_coupling.run_scenario_F4()
+    # scenarios_sector_coupling.revert_inputs_changes()
 
-    scenarios_sector_coupling.run_scenario_E1()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_E2()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_E3()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_E4()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_E5()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_E6()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_F1()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_F2()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_F3()
-    scenarios_sector_coupling.revert_inputs_changes()
-    scenarios_sector_coupling.run_scenario_F4()
-    scenarios_sector_coupling.revert_inputs_changes()
+    scenarios_gas = ScenariosGas()
+    scenarios_gas.setup_class()
+    # scenarios_gas.run_scenario_G1()
+    # scenarios_gas.revert_inputs_changes()
+    # scenarios_gas.run_scenario_G2()
+    # scenarios_gas.revert_inputs_changes()
+    scenarios_gas.run_scenario_G3()
+    scenarios_gas.revert_inputs_changes()
+    scenarios_gas.run_scenario_G4()
+    scenarios_gas.revert_inputs_changes()
